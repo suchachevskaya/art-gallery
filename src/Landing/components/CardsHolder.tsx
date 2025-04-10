05d40d02df44e281105448ebd607054640258e71
r0uven <roma-wolf2002@mail.ru>
2025-04-10 14:53:57 +0300

    feat: add art card component with title, author and image

--- src/Landing/components/CardsHolder.tsx
+++ src/Landing/components/CardsHolder.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import Card from "../../components/Card";
 
 export default function CardsHolder() {
-    const cardsData = [1, 2, 3, 4, 5]; // Массив, можно заменить на данные из API, стейта и т.п.
+  const cardsData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [showAll, setShowAll] = useState(false);
 
-    return (
-        <div className="cards-holder-wrapper">
-            <div className="card-holder">
-                {cardsData.map((data, index) => (
-                    <Card key={index} /> // Лучше, если будет уникальный ключ
-                ))}
-            </div>
-            <button className="show-all-button">Показать все</button>
-        </div>
+  const visibleCards = showAll ? cardsData : cardsData.slice(0, 3);
 
-    );
+  return (
+    <div className="cards-holder-wrapper">
+      <div className="card-holder">
+        {visibleCards.map((data, index) => (
+          <Card
+            key={index}
+            title="The Starry Night"
+            author="Vincent van Gogh"
+            imageUrl="https://upload.wikimedia.org/wikipedia/commons/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+          />
+        ))}
+      </div>
+
+      {!showAll && (
+        <button
+          className="show-all-button"
+          onClick={() => setShowAll(true)}
+        >
+          Показать все
+        </button>
+      )}
+    </div>
+  );
 }
-
-
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,27 @@
-import './Card.scss';
+import style from './Card.module.scss';
+import FavIcon from './FavIcon';
 
-export default function Description() {
+
+type ArtCardProps = {
+    title: string;
+    author: string;
+    imageUrl: string;
+};
+
+export default function Card({ title, author, imageUrl }: ArtCardProps) {
     return (
-        <div>
-            <div className="card">Здесь будет карточка</div>
+        <div className={style.card}>
+        <img src={imageUrl} alt={title} className={style.Art} />
+        <div className={style.content}>
+          <div className={style.header}>
+            <div>
+              <h2 className={style.title}>{title}</h2>
+              <p className={style.author}>{author}</p>
+            </div>
+            <button className={style.favButton} aria-label="Добавить в избранное"><FavIcon ></FavIcon></button>
+          </div>
         </div>
+      </div>
     )
 }
+
