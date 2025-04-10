3568ab3c991b203a8096302656fe1cc7644f6d5b
suchachevskaya <116747974+suchachevskaya@users.noreply.github.com>
2025-04-10 11:27:01 +0300

    Рефакторинг роутинга
    Рефакторинг роутинга для устранения повторения Header:
    - Переместила Header для глобального отображения на всех маршрутах
    - Убрала дублирующийся код Header в каждом маршруте
    - Улучшена структура проекта для удобства поддержки

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,56 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Favorites from "./favorites/Favorites";
 import History from "./history/History";
 import Header from "./header/Header";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header /> <h1>main</h1>
-      </>
-    ),
-  },
-  {
-    path: "/history",
-    element: (
-      <>
-        <Header /> <History />
-      </>
-    ),
-  },
-  {
-    path: "/favorites",
-    element: (
-      <>
-        <Header /> <Favorites />
-      </>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <Header /> <h1>login</h1>
-      </>
-    ),
-  },
-  {
-    path: "/registration",
-    element: (
-      <>
-        <Header /> <h1>registration</h1>
-      </>
-    ),
-  },
-]);
+
 function App() {
   return (
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>main</h1>
+            </>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <>
+              <History />
+            </>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <>
+              <Favorites />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <div>login</div>
+            </>
+          }
+        />
+        <Route
+          path="/registration"
+          element={
+            <>
+              <div>registration</div>
+            </>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
