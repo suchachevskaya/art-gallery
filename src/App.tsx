b55d168670038a88787edd4186108e4a0105fc30
r0uven <roma-wolf2002@mail.ru>
2025-04-10 22:57:00 +0300

    refactor: replace hardcoded route path with ROUTES constant

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { ErrorBoundary } from "./ErrorBoundary"
 import { Routes, Route, Link } from 'react-router-dom';
 
 export function App() {
+  const ROUTES = { home: '/'}
 
   return (
     <>
       {/* <Shapka></Shapka> */}
       <ErrorBoundary fallback={<h2>Что-то пошло не так...</h2>}>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path={ROUTES.home} element={<Landing />} />
         </Routes>
       </ErrorBoundary>
     </>
